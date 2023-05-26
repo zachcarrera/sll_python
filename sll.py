e7f5cc90afb58999df0297f24e97ee82d1b011d2
Zach Carrera <zachcarrera@gmail.com>
2023-05-25 23:36:11 -0700

    create remove_from_back method

--- sll.py
+++ sll.py
@@ -39,6 +39,23 @@ class SList:
         self.head = self.head.next
         return temp.value
 
+    def remove_from_back(self):
+        """remove the node at the back of the list"""
+
+        if self.head is None:
+            return
+
+        if self.head.next is None:
+            return self.remove_from_front()
+
+        runner = self.head
+        # move runner to the second to last node
+        while runner.next.next is not None:
+            runner = runner.next
+        value = runner.next.value
+        runner.next = None
+        return value
+
     def print_list(self):
         """print the singly linked list with each node on line"""
 
@@ -62,5 +79,6 @@ sll_1.print_list()
 
 print("-----")
 print(sll_1.remove_from_front())
+print(sll_1.remove_from_back())
 print("-----")
 sll_1.print_list()
