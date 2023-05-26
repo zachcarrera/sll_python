a35033415239ac4d6477b329b39687d601d29a8c
Zach Carrera <zachcarrera@gmail.com>
2023-05-25 22:30:26 -0700

    create SList and the print_list method

--- sll.py
+++ sll.py
@@ -2,3 +2,18 @@ class SLNode:
     def __init__(self, val):
         self.value = val
         self.next = None
+
+
+class SList:
+    def __init__(self):
+        self.head = None
+
+    def print_list(self):
+        """print the singly linked list with each node on line"""
+
+        runner = self.head
+        while runner is not None:
+            print(runner.value)
+            runner = runner.next
+
+        return self
